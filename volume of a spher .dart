59fd2d2433a31f6d8177761fe3b0cc278114b95d
Esraa Alaa Elden <122882067+Esraaalaaelden@users.noreply.github.com>
2023-02-24 18:04:22 +0200

    Create  volume of a spher .dart

---
+++ volume of a spher .dart
@@ -0,0 +1,8 @@
+void main(){
+  
+  const pi = 3.14;
+  var radius = 6 ;
+  var  radius = stdin.readLineSync();
+  radius = double.parse(radius);
+  print('The area is: ${ 4/3 *pi*radius*radius*radius}');
+}
